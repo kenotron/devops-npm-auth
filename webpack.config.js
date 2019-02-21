b3932ba25a85b131615fdd23e0a4b9364a4b3ffc
Ken <kchau@microsoft.com>
2019-02-21 09:21:38 -0800

    creating a no-dep version of the EXCELLENT better-vsts-npm-auth package

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: 'shebang-loader!better-vsts-npm-auth/dist/cli.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'devops-npm-auth.js'
+  }
+};
